21dca4ad5ec0b05bb182e3da2c5a642134ac939e
Daniel A. Wagenaar <daw@caltech.edu>
2024-07-08 23:36:01 -0700

    Older bugfix to Struct

--- src/escope_wagenadl/escopelib/Struct.py
+++ src/escope_wagenadl/escopelib/Struct.py
@@ -2,7 +2,7 @@ class Struct(object):
     def __repr__(self):
         s0 = super(Struct,self).__repr__()
         s1 = ' {'
-        kk=self.__dict__.keys()
+        kk=list(self.__dict__.keys())
         kk.sort()
         for k in kk:
             s1 += k + '=' + repr(self.__dict__[k]) + ', '
@@ -10,7 +10,7 @@ class Struct(object):
     
     def __str__(self):
         s1='{'
-        kk=self.__dict__.keys()
+        kk=list(self.__dict__.keys())
         kk.sort()
         for k in kk:
             s1 += k + '=' + str(self.__dict__[k]) + ', '
